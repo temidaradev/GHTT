1c4662b66b744990a2383044e61d930ab457330c
lidldev <ahmet.affan.ebcioglu@outlook.com>
2024-10-20 16:13:43 +0300

    template done

--- views/layouts/base_templ.go
+++ views/layouts/base_templ.go
@@ -51,6 +51,10 @@ func Base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = components.Logos().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		templ_7745c5c3_Err = components.Footer().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
